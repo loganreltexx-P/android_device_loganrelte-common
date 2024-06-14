4dca26e4306faf5764f349047d688f5ce381e38b
NotNoelChannel <52098241+NotNoelChannel@users.noreply.github.com>
2024-06-14 14:55:26 +0200

    Update loganrelte-common.mk

--- loganrelte-common.mk
+++ loganrelte-common.mk
@@ -13,9 +13,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# Also get non-open-source specific aspects
-$(call inherit-product, $(COMMON_VENDOR_PATH)/serrano-common-vendor.mk)
-
 # Common overlay
 DEVICE_PACKAGE_OVERLAYS += $(COMMON_PATH)/overlay
 
